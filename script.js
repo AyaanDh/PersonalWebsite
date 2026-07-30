5a20ebee3a7c51391ca9ed0bdb974156833818cf
Ayaan Dhalait <ayaan@ayaan.is-a.dev>
2026-07-30 11:37:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,238 +1,146 @@
-const words = [
-"Software Developer",
-"C++ Programmer",
-"Python Builder",
-"Future Computer Scientist",
-"System Thinker"
-];
+const username = "ayaandh";
 
-const typewriter = document.getElementById("typewriter");
+async function loadGithubDashboard(){
 
-let wordIndex = 0;
-let charIndex = 0;
-let deleting = false;
+    const user = await fetch(
+        `https://api.github.com/users/${username}`
+    ).then(r => r.json());
 
-function type(){
-const current = words[wordIndex];
 
-if(!deleting){
-typewriter.textContent = current.substring(0,charIndex++);
-}else{
-typewriter.textContent = current.substring(0,charIndex--);
+    document.querySelector("#avatar").src = user.avatar_url;
+
+    document.querySelector("#username").textContent =
+        user.name || username;
+
+    document.querySelector("#bio").textContent =
+        user.bio || "Building software and open-source projects.";
+
+
+    const repos = await fetch(
+        `https://api.github.com/users/${username}/repos?per_page=100`
+    ).then(r => r.json());
+
+
+    let stars = 0;
+    let forks = 0;
+    let languages = {};
+
+
+    repos.forEach(repo => {
+
+        stars += repo.stargazers_count;
+
+        forks += repo.forks_count;
+
+
+        if(repo.language){
+
+            languages[repo.language] =
+                (languages[repo.language] || 0) + 1;
+
+        }
+
+    });
+
+
+    document.querySelector("#repos").textContent =
+        user.public_repos;
+
+    document.querySelector("#followers").textContent =
+        user.followers;
+
+    document.querySelector("#stars").textContent =
+        stars;
+
+    document.querySelector("#forks").textContent =
+        forks;
+
+
+    const sortedLanguages = Object.entries(languages)
+        .sort((a,b)=>b[1]-a[1])
+        .slice(0,5);
+
+
+    const totalLanguages = sortedLanguages.reduce(
+        (sum, lang)=>sum + lang[1],
+        0
+    );
+
+
+    const languageContainer =
+        document.querySelector("#language-list");
+
+
+    languageContainer.innerHTML = "";
+
+
+    sortedLanguages.forEach(lang => {
+
+        const percentage = Math.round(
+            (lang[1] / totalLanguages) * 100
+        );
+
+
+        languageContainer.innerHTML += `
+
+        <div class="language-row">
+
+            <div class="language-name">
+
+                <span>${lang[0]}</span>
+
+                <span>${percentage}%</span>
+
+            </div>
+
+
+            <div class="bar">
+
+                <div style="width:${percentage}%"></div>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+
+    const recentContainer =
+        document.querySelector("#recent-projects");
+
+
+    recentContainer.innerHTML = "";
+
+
+    repos
+        .sort(
+            (a,b)=>
+            new Date(b.updated_at)
+            -
+            new Date(a.updated_at)
+        )
+        .slice(0,4)
+        .forEach(repo => {
+
+            recentContainer.innerHTML += `
+
+            <a href="${repo.html_url}" target="_blank">
+
+                <h4>${repo.name}</h4>
+
+                <p>
+                    ${repo.description || "No description available."}
+                </p>
+
+            </a>
+
+            `;
+
+        });
+
 }
 
-let speed = deleting ? 40 : 80;
 
-if(!deleting && charIndex === current.length + 1){
-deleting = true;
-speed = 1200;
-}
-
-if(deleting && charIndex === 0){
-deleting = false;
-wordIndex = (wordIndex + 1) % words.length;
-speed = 300;
-}
-
-setTimeout(type,speed);
-}
-
-type();
-
-const nav = document.querySelector("nav");
-const hamburger = document.querySelector(".hamburger");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-if(hamburger){
-hamburger.addEventListener("click",()=>{
-hamburger.classList.toggle("active");
-mobileMenu.classList.toggle("active");
-});
-}
-
-document.querySelectorAll(".mobile-menu a").forEach(link=>{
-link.addEventListener("click",()=>{
-hamburger.classList.remove("active");
-mobileMenu.classList.remove("active");
-});
-});
-
-window.addEventListener("scroll",()=>{
-if(window.scrollY > 40){
-nav.classList.add("scrolled");
-}else{
-nav.classList.remove("scrolled");
-}
-});
-
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav-links a");
-
-window.addEventListener("scroll",()=>{
-let current = "";
-
-sections.forEach(section=>{
-const top = section.offsetTop - 150;
-if(window.scrollY >= top){
-current = section.getAttribute("id");
-}
-});
-
-navLinks.forEach(link=>{
-link.classList.remove("active");
-if(link.getAttribute("href") === "#" + current){
-link.classList.add("active");
-}
-});
-});
-
-const reveal = document.querySelectorAll(".project-card,.certificate-card,.stack-group,.step,.exp-card,.edu-card");
-
-const observer = new IntersectionObserver(entries=>{
-entries.forEach(entry=>{
-if(entry.isIntersecting){
-entry.target.classList.add("visible");
-}
-});
-},{threshold:0.15});
-
-reveal.forEach(el=>{
-observer.observe(el);
-});
-
-const canvas = document.getElementById("bg");
-const ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let particles = [];
-
-for(let i=0;i<60;i++){
-particles.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height,
-r:Math.random()*2,
-dx:(Math.random()-0.5)*0.5,
-dy:(Math.random()-0.5)*0.5
-});
-}
-
-function animate(){
-ctx.clearRect(0,0,canvas.width,canvas.height);
-
-for(let p of particles){
-p.x += p.dx;
-p.y += p.dy;
-
-if(p.x < 0 || p.x > canvas.width) p.dx *= -1;
-if(p.y < 0 || p.y > canvas.height) p.dy *= -1;
-
-ctx.beginPath();
-ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
-ctx.fillStyle = "rgba(255,106,0,0.3)";
-ctx.fill();
-}
-
-requestAnimationFrame(animate);
-}
-
-animate();
-
-window.addEventListener("resize",()=>{
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-});
-
-const cursor = document.querySelector(".cursor");
-const dot = document.querySelector(".cursor-dot");
-
-let mx = 0, my = 0, cx = 0, cy = 0;
-
-document.addEventListener("mousemove",e=>{
-mx = e.clientX;
-my = e.clientY;
-});
-
-function animateCursor(){
-cx += (mx - cx) * 0.12;
-cy += (my - cy) * 0.12;
-
-if(cursor){
-cursor.style.left = cx + "px";
-cursor.style.top = cy + "px";
-}
-
-if(dot){
-dot.style.left = mx + "px";
-dot.style.top = my + "px";
-}
-
-requestAnimationFrame(animateCursor);
-}
-
-animateCursor();
-
-const about = document.querySelector(".about-card");
-
-if(about){
-document.addEventListener("mousemove",(e)=>{
-const r = about.getBoundingClientRect();
-
-const x = (e.clientX - r.left) / r.width;
-const y = (e.clientY - r.top) / r.height;
-
-about.style.transform = `
-perspective(1000px)
-rotateX(${(y-0.5)*6}deg)
-rotateY(${(x-0.5)*-6}deg)
-translateY(-5px)
-`;
-});
-}
-
-emailjs.init("801l6SE6e-74lFPOB");
-
-const form = document.querySelector(".contact-form");
-const popup = document.getElementById("successPopup");
-const btnText = document.getElementById("btnText");
-
-if(form){
-form.addEventListener("submit",async (e)=>{
-e.preventDefault();
-
-if(btnText) btnText.textContent = "Sending...";
-
-try{
-
-await emailjs.sendForm(
-"service_fewot77",
-"template_nvasqqd",
-form
-);
-
-await emailjs.sendForm(
-"service_fewot77",
-"template_xlzufoi",
-form
-);
-
-if(btnText) btnText.textContent = "Sent";
-
-if(popup){
-popup.classList.add("show");
-
-setTimeout(()=>{
-popup.classList.remove("show");
-if(btnText) btnText.textContent = "Send Message";
-},2500);
-}
-
-form.reset();
-
-}catch(err){
-console.log(err);
-if(btnText) btnText.textContent = "Try Again";
-}
-});
-}
+loadGithubDashboard();
